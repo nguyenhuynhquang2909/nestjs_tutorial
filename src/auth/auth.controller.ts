57dc9d0adeba5fcfa6b95300d70d92f840eb990c
nguyenhuynhquang2909 <s3927039@rmit.edu.vn>
2024-02-28 16:37:05 +0700

    Finish sign up logic

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -3,14 +3,14 @@ import { sign } from "crypto";
 import { AuthService } from "./auth.service";
 import { AuthDto } from "./dto";
 
-@Controller()
+@Controller('auth')
 export class AuthController {
     constructor(private authService: AuthService) {
     }
 
     @Post('signup')
     signup(@Body() dto: AuthDto) {
-        return this.authService.signup()
+        return this.authService.signup(dto)
     };
 
     @Post('login')
